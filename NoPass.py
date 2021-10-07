1a64c4b4cde6b6a74868a3000e4188ac5ec106c2
Igor <39525496+misakigr@users.noreply.github.com>
2021-10-07 12:22:27 +0500

    Add files via upload
    Убрали лишние скобки и n, который равнялся нулю.

--- NoPass.py
+++ NoPass.py
@@ -1,40 +1,58 @@
+#!/usr/bin/env python
+"""Creates remote backups.
+
+It let's remote machines stream it's data to your machine using the SSH connection.
+There are plugins available that provide functionality.
+"""
+
 import hashlib
 
 from tkinter import *
+
+__author__ = "Igor V. Misak"
+__copyright__ = "Copyright 2021, Debian SSH Backup"
+__credits__ = ["Elena Kozlova", "Maxim Stravinsky"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Igor V. Misak"
+__email__ = "misakigr@mail.ru"
+__status__ = "Production"
+
+
 salt = ''
-def clicked():  
-    res = "{}".format(txt.get())  
+def clicked():
+    res = "{}".format(txt.get())
     lbl.configure(text=res)
     global salt
     salt = res
     print(salt)
-    res1 = "{}".format(txt1.get())  
+    res1 = "{}".format(txt1.get())
     lbl1.configure(text=res1)
     global login
     login = res1
     print(login)
-    window.destroy()    
-  
-  
-window = Tk()  
-window.title("NoPass")  
-window.geometry('300x50') 
+    window.destroy()
+
+
+window = Tk()
+window.title("NoPass")
+window.geometry('300x50')
 x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2
 y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2
-window.wm_geometry("+%d+%d" % (x, y)) 
-lbl = Label(window, text="Введите соль:")  
-lbl.grid(column=0, row=0)  
+window.wm_geometry("+%d+%d" % (x, y))
+lbl = Label(window, text="Введите соль:")
+lbl.grid(column=0, row=0)
 txt = Entry(window,width=15)
-txt.focus()  
+txt.focus()
 txt.grid(column=1, row=0)
-lbl1 = Label(window, text="Введите Лог:")  
-lbl1.grid(column=0, row=2)  
+lbl1 = Label(window, text="Введите Лог:")
+lbl1.grid(column=0, row=2)
 txt1 = Entry(window,width=15)
-txt1.focus()  
+txt1.focus()
 txt1.grid(column=1, row=2)
 
-  
-btn = Button(window, text="Клик!", command=clicked)  
+
+btn = Button(window, text="Клик!", command=clicked)
 btn.grid(column=2, row=0)
 
 window.mainloop()
@@ -59,7 +77,7 @@ salt =salt.encode()
 
 login = login.encode()
 
-key = hashlib.pbkdf2_hmac('sha256', login, salt, 1, 16)
+key = hashlib.pbkdf2_hmac('sha256', login, salt, 100000, 16)
 
 key = int(key.hex(),16)
 
@@ -69,26 +87,26 @@ print (key)
 
 n=0
 
-p+=(leters_2[(int(key[(n):(n+1)])):((int(key[(n):(n+1)]))+1)])
-p+=(leters_2[(int(key[(n+1):(n+2)])):((int(key[(n+1):(n+2)]))+1)])
-p+=(leters[(int(key[(n+2):(n+3)])):((int(key[(n+2):(n+3)]))+1)])
-p+= (digits[(int(key[(n+3):(n+4)])):((int(key[(n+3):(n+4)]))+1)])
+p+=(leters_2[(int(key[0:1])):((int(key[0:1]))+1)])
+p+=(leters_2[(int(key[1:2])):((int(key[1:2]))+1)])
+p+=(leters[(int(key[2:3])):((int(key[2:3]))+1)])
+p+= (digits[(int(key[3:4])):((int(key[3:4]))+1)])
 
-p+=(leters[(int(key[(n+4):(n+5)])+9):((int(key[(n+4):(n+5)]))+10)])
-p+= (digits[(int(key[(n+5):(n+6)])):((int(key[(n+5):(n+6)]))+1)])
+p+=(leters[(int(key[4:5])+9):((int(key[4:5]))+10)])
+p+= (digits[(int(key[5:6])):((int(key[5:6]))+1)])
 
-p+=(leters_2[(int(key[(n+6):(n+7)])+9):((int(key[(n+6):(n+7)]))+10)])
-p+=(symbols[(int(key[(n+7):(n+8)])):((int(key[(n+7):(n+8)]))+1)])
-p+=(leters_2[(int(key[(n+9):(n+10)])+16):((int(key[(n+9):(n+10)]))+17)])
+p+=(leters_2[(int(key[6:7])+9):((int(key[6:7]))+10)])
+p+=(symbols[(int(key[7:8])):((int(key[7:8]))+1)])
+p+=(leters_2[(int(key[9:10])+16):((int(key[9:10]))+17)])
 
-p+= (digits[(int(key[(n+11):(n+12)])):((int(key[(n+11):(n+12)]))+1)])
-p+=(leters[(int(key[(n+13):(n+14)])+16):((int(key[(n+13):(n+14)]))+17)])
-p+=(leters[(int(key[(n+14):(n+15)])+16):((int(key[(n+14):(n+15)]))+17)])
+p+= (digits[(int(key[11:12])):((int(key[11:12]))+1)])
+p+=(leters[(int(key[13:14])+16):((int(key[13:14]))+17)])
+p+=(leters[(int(key[14:15])+16):((int(key[14:15]))+17)])
 print()
 print(p)
 
 root = Tk()
-root.title("NoPass")  
+root.title("NoPass")
 root.geometry('200x50')
 x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
 y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
